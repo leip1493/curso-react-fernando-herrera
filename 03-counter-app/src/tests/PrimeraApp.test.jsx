e6e35c0b86c8ee4e1149eb8cbfa76ffe2372127e
Luis Izquierdo <luis.izquierdo@sitiouno.com>
2020-07-07 01:11:31 -0400

    Test para encontrar elemento dentro del componente

--- 03-counter-app/src/tests/PrimeraApp.test.jsx
+++ 03-counter-app/src/tests/PrimeraApp.test.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import '@testing-library/jest-dom';
-import { shallow } from 'enzyme';
+import React from "react";
+import "@testing-library/jest-dom";
+import { shallow } from "enzyme";
 
-import PrimeraApp from '../PrimeraApp';
-describe('Pruebas en <PrimeraApp />', () => {
-  
+import PrimeraApp from "../PrimeraApp";
+describe("Pruebas en <PrimeraApp />", () => {
   // test('debe mostrar el mensaje "Hola Mundo"', () => {
   //   const saludo = "Hola Mundo";
 
@@ -12,16 +11,27 @@ describe('Pruebas en <PrimeraApp />', () => {
 
   //   expect( getByText(saludo) ).toBeInTheDocument();
   // })
-  
-  test('debe mostrar <PrimeraApp /> correctamente', () => {
-    
-    const saludo = "Hola Mundo"
+
+  test("debe mostrar <PrimeraApp /> correctamente", () => {
+    const saludo = "Hola Mundo";
 
     const wrapper = shallow(<PrimeraApp saludo={saludo} />);
-    
+
     expect(wrapper).toMatchSnapshot();
-
   });
-  
 
-})
+  test("debe mostrar el subtitulo enviado por props", () => {
+    const saludo = "Hola Mundo";
+    const subtitulo = "Soy un subtitulo";
+
+    const wrapper = shallow(
+      <PrimeraApp saludo={saludo} subtitulo={subtitulo} />
+    );
+
+    const textoParrafo = wrapper.find("p").text();
+
+    console.log(textoParrafo)
+
+    expect(textoParrafo).toBe(subtitulo);
+  });
+});
