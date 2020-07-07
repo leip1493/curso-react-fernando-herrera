30d17c011ae815062683e3114fa9cd81e141bd24
Luis Izquierdo <luis.izquierdo@sitiouno.com>
2020-07-07 01:40:24 -0400

    Se agregan test al ejecutar acciones click

--- 03-counter-app/src/tests/CounterApp.test.jsx
+++ 03-counter-app/src/tests/CounterApp.test.jsx
@@ -4,8 +4,13 @@ import { shallow } from "enzyme";
 import CounterApp from "../CounterApp";
 
 describe("Pruebas en <CounterApp />", () => {
+  let wrapper = shallow(<CounterApp />);// Redundante , pero habilita el intellisense :P
+
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);
+  })
+
   test("Debe mostrar <CounterApp /> correctamente ", () => {
-    const wrapper = shallow(<CounterApp />);
 
     expect(wrapper).toMatchSnapshot();
   });
@@ -13,12 +18,59 @@ describe("Pruebas en <CounterApp />", () => {
   test("Debe mostrar mostrar el valor por defecto de 100 ", () => {
     const initialCounter = 100;
 
-    const wrapper = shallow(
-      <CounterApp value={initialCounter}/>
-    );
-
-    const counterText = wrapper.find("h2").text().trim();
+    const wrapper = shallow(<CounterApp value={initialCounter} />);
+    
+    const counterText = wrapper
+      .find("h2")
+      .text()
+      .trim();
 
     expect(counterText).toBe(initialCounter.toString());
   });
+
+  test('debe incrementar el valor a 11 con el boton +1 ', () => {
+  
+    const incrementButton = wrapper.find('button').at(0);
+    incrementButton.simulate('click', {});
+    
+    const counterText = wrapper
+      .find("h2")
+      .text()
+      .trim();
+
+    expect(counterText).toBe('11');
+  })
+
+  test('debe decrementar el valor a 9 con el boton -1 ', () => {
+  
+    const decrementButton = wrapper.find('button').at(2);
+    decrementButton.simulate('click', {});
+
+    const counterText = wrapper
+      .find("h2")
+      .text()
+      .trim();
+
+    expect(counterText).toBe('9');
+  });
+
+  test('debe resetear el valor inicial al hacer click en el boton reset', () => {
+    const initialCounter = 50;
+
+    const wrapper = shallow(<CounterApp value={initialCounter} />);
+
+    const decrementButton = wrapper.find('button').at(2);
+    decrementButton.simulate('click', {});
+
+    const resetButton = wrapper.find('button').at(1);
+    resetButton.simulate("click");
+
+    const counterText = wrapper
+      .find("h2")
+      .text()
+      .trim();
+
+    expect(counterText).toBe(initialCounter.toString());
+  });
+  
 });
