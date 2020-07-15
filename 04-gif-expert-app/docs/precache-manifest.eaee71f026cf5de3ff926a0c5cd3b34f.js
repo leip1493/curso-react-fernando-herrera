1dcb84e5387ab87abe328653584d948aa26fc988
Luis Izquierdo <luis.izquierdo@sitiouno.com>
2020-07-15 00:32:30 -0400

    Docs agregado para github pages

---
+++ 04-gif-expert-app/docs/precache-manifest.eaee71f026cf5de3ff926a0c5cd3b34f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66955fc891260aa82aec4bfc192473fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ee6291b80c90556b660",
+    "url": "/static/css/main.034c1573.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee6291b80c90556b660",
+    "url": "/static/js/main.6ba22703.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
