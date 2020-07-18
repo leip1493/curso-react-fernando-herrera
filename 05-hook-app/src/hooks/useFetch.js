9813378e2044e0e432d2f078da292bfddc469f35
Luis Izquierdo <luis.izquierdo@sitiouno.com>
2020-07-17 21:54:34 -0400

    Se crea hook useFetch que recibe url  en conjunto con otro custom hook

---
+++ 05-hook-app/src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = url => {
+  const [state, setstate] = useState({
+    data: null,
+    loading: true,
+    error: null
+  });
+
+  useEffect(() => {
+    setstate({
+      data: null,
+      loading: true,
+      error: null
+    });
+
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        setstate({
+          loading: false,
+          error: null,
+          data
+        });
+      });
+  }, [url]);
+
+  return state;
+};
