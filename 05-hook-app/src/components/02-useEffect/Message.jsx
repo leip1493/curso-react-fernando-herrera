8c196eb6c0911e823917bf7805e0c46cff523e82
Luis Izquierdo <luis.izquierdo@sitiouno.com>
2020-07-17 21:20:47 -0400

    cleanup de un event listener

--- 05-hook-app/src/components/02-useEffect/Message.jsx
+++ 05-hook-app/src/components/02-useEffect/Message.jsx
@@ -1,17 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const { x, y } = coords;
   useEffect(() => {
-    console.log("Componente message montado");
+    const mouseMove = e => {
+      const coords = { x: e.x, y: e.y };
+      setCoords(coords);
+    };
+
+    window.addEventListener("mousemove", mouseMove);
 
     return () => {
-      console.log("Componente message desmontado");
+      window.removeEventListener("mousemove", mouseMove);
     };
   }, []);
 
   return (
     <div>
       <h3>Soy un mensaje!</h3>
+      <p>
+        x: {x} y: {y}
+      </p>
     </div>
   );
 };
