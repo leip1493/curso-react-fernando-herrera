333edadef77424617364f0a7e04845d7284697d7
Luis Izquierdo <luis.izquierdo@sitiouno.com>
2020-09-08 22:29:07 -0400

    Utilizando el dispatch de la accion del useReducer

--- 05-hook-app/src/components/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/components/08-useReducer/TodoApp.jsx
@@ -12,19 +12,64 @@ const initialState = [
 ];
 
 export const TodoApp = () => {
-  const [todos] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
 
-  console.log(todos);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      description: "Nueva tarea",
+      done: false,
+    };
+
+    const action = {
+      type: "add",
+      payload: newTodo,
+    };
+
+    dispatch(action);
+  };
+
   return (
     <div>
-      <h1>TodoApp</h1>
+      <h1>TodoApp: ({todos.length})</h1>
       <hr />
 
-      <ul>
-        <li>Hola</li>
-        <li>Mundo</li>
-        <li>Hola de nuevo</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, index) => (
+              <li key={todo.id} className="list-group-item">
+                <p className="text-center">
+                  {index + 1}. {todo.description}
+                </p>
+                <button className="btn btn-danger">Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+
+          <form onSubmit={handleSubmit}>
+            <input
+              className="form-control"
+              type="text"
+              name="description"
+              placeholder="Aprender ..."
+              autoComplete="off"
+            />
+            <button
+              className="btn btn-outline-primary btn-block mt-1"
+              type="submit"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
