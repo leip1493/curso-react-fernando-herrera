beb67f662002efc31272651f9c656d51ccbd51b2
Luis Izquierdo <luis.izquierdo@sitiouno.com>
2020-09-08 20:35:50 -0400

    useCallback - Para garantizar el no renderizado y cuando se tiene dependencia de props que son funciones combinarlo con React.memo

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -9,10 +9,10 @@ import ReactDOM from "react-dom";
 // import { FocusScreen } from "./components/04-useRef/FocusScreen";
 // import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
 // import { Memorize } from "./components/06-memos/Memorize";
-import { MemoHook } from "./components/06-memos/MemoHook";
+// import { MemoHook } from "./components/06-memos/MemoHook";
+import { CallbackHook } from "./components/06-memos/CallbackHook";
 
 ReactDOM.render(
-  <MemoHook />,
-
+  <CallbackHook />,
   document.getElementById("root")
 );
