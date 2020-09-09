cc8abdabee596c1d59f0b859d0b069c6c26697b9
Luis Izquierdo <luis.izquierdo@sitiouno.com>
2020-09-08 20:06:29 -0400

    useMemo para evitar los proceso pesados en base a las dependencias

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -8,10 +8,11 @@ import ReactDOM from "react-dom";
 // import { MultipleCustomHooks } from "./components/03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./components/04-useRef/FocusScreen";
 // import { RealExampleRef } from "./components/04-useRef/RealExampleRef";
-import { Memorize } from "./components/06-memos/Memorize";
+// import { Memorize } from "./components/06-memos/Memorize";
+import { MemoHook } from "./components/06-memos/MemoHook";
 
 ReactDOM.render(
-  <Memorize />,
+  <MemoHook />,
 
   document.getElementById("root")
 );
