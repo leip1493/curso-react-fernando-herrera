042e2e5efaa279168de7a90ba99ee8cc92f1b166
Luis Izquierdo <luis.izquierdo@sitiouno.com>
2020-09-10 21:49:11 -0400

    dispatch  toggle

--- 05-hook-app/src/components/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/components/08-useReducer/TodoApp.jsx
@@ -36,6 +36,13 @@ export const TodoApp = () => {
     dispatch(deleteAction(todoId));
   };
 
+  const handleToggle = (todoId) => {
+    dispatch({
+      type: "toggle",
+      payload: todoId,
+    });
+  };
+
   return (
     <div>
       <h1>TodoApp: ({todos.length})</h1>
@@ -46,7 +53,10 @@ export const TodoApp = () => {
           <ul className="list-group list-group-flush">
             {todos.map((todo, index) => (
               <li key={todo.id} className="list-group-item">
-                <p className="text-center">
+                <p
+                  className={`${todo.done && "complete"}`}
+                  onClick={() => handleToggle(todo.id)}
+                >
                   {`${index + 1}. ${todo.description}`}
                 </p>
                 <button
