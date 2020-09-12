f38d41511b6a3209d5921da843da8a2210259069
Luis Izquierdo <luis.izquierdo@sitiouno.com>
2020-09-11 20:59:41 -0400

    createContext y useContext

--- 05-hook-app/src/components/09-useContext/HomeScreen.jsx
+++ 05-hook-app/src/components/09-useContext/HomeScreen.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
 
 export const HomeScreen = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <div>
       <h1>HomeScreen</h1>
-      <hr/>
+      <hr />
+      <pre className="container">{JSON.stringify(user, null, 4)}</pre>
     </div>
-  )
-}
+  );
+};
